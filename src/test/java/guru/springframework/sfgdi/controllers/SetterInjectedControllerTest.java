a7eeb3ca4b26fafe2069056ec181a0ea32afebe1
aleks-p <geniusap@gmail.com>
2020-07-04 14:38:33 +0200

    Add dependency injection examples

---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    private SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void testGetGreeting() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
